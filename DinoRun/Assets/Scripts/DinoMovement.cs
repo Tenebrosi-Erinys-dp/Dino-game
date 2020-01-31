cafa6a04dca2c14b5f3b9ed853b51c716f72b8ec
alexdzius <alex.dzius@digipen.edu>
2020-01-31 13:07:07 -0800

    Everything so far

---
+++ DinoRun/Assets/Scripts/DinoMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoMovement : MonoBehaviour
+{
+    Rigidbody2D dino;
+    bool canJump = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dino = GetComponent<Rigidbody2D>();
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+        // if dino hits literally something -- TODO: add fix to only with object of type sprite
+        if (collision.gameObject.GetComponent<MapMovement>() != null)
+        {
+            // check if object is sprite
+
+            // display death screen & option to restart
+
+            // display highscore
+
+            // remove controls
+            
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        canJump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sensitivity = 0.00001f;
+        // if jump
+        if (canJump == true)
+        {
+            if (Input.GetAxis("Vertical") > sensitivity)
+            {
+                // jump  velcotiy
+                dino.velocity = new Vector3(0, 5, 0);
+            }
+        }
+        // if duck
+        if (Input.GetAxis("Vertical") < -sensitivity)
+        {
+            // change to duck animation
+            // TODO: implement duck animation
+        }
+    }
+}
---
+++ DinoRun/Assets/Scripts/TerrainGeneration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGeneration : MonoBehaviour
+{
+    public GameObject[] chunks;
+    public int random;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+       
+    // Update is called once per frame
+    void Update()
+    {
+       generateTerrain();
+    }
+    void generateTerrain()
+    {
+        random = Random.Range(1, chunks.Length - 1);
+        Instantiate(chunks[random], new Vector3(14, -3, 0), Quaternion.identity);
+    }
+}
